a6fb5e82b2fd98389be4ce23f77bd4731ab63579
nicolasdumitru <nicolas@ndumitru.com>
2025-05-26 18:43:43 +0300

    Initialize flake

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "OxiTal";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [
+            "rust-analyzer"
+            "rust-docs"
+            "rust-src"
+          ];
+        };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              rustToolchain
+            ];
+          };
+      }
+    );
+}
